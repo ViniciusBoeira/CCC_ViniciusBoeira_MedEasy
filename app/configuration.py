3042301c03390fb3d23362cfdaf323c6b4435eea
Vinicius <vini.virtuoso28@gmail.com>
2025-10-12 19:11:30 -0300

    Atualizei a configuração para usar variáveis de ambiente e ajustei a documentação para refletir as mudanças.

--- app/configuration.py
+++ app/configuration.py
@@ -1,26 +1,22 @@
+import os
 
 class Config(object):
-	"""
-	Configuration base, for all environments.
-	"""
-	DEBUG = False
-	TESTING = False
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///application.db'
-	BOOTSTRAP_FONTAWESOME = True
-	SECRET_KEY = "MINHACHAVESECRETA"
-	CSRF_ENABLED = True
-	SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-	#Get your reCaptche key on: https://www.google.com/recaptcha/admin/create
-	#RECAPTCHA_PUBLIC_KEY = "6LffFNwSAAAAAFcWVy__EnOCsNZcG2fVHFjTBvRP"
-	#RECAPTCHA_PRIVATE_KEY = "6LffFNwSAAAAAO7UURCGI7qQ811SOSZlgU69rvv7"
-
+    """
+    Configuração base, para todos os ambientes.
+    """
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://meduser:password@localhost:5432/medeasy')
+    BOOTSTRAP_FONTAWESOME = True
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'password')
+    CSRF_ENABLED = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = 'mysql://user@localhost/foo'
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
-	DEBUG = True
+    DEBUG = True
 
 class TestingConfig(Config):
-	TESTING = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
