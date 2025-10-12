77c574c6643faf816d78942e011e92aaa2d77d15
Vinicius <vini.virtuoso28@gmail.com>
2025-10-12 19:07:36 -0300

    Alterei run.py para simplificar a inicialização do aplicativo e adicionar uma rota hello world

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-import os
-from app import app
+from flask import Flask
 
+app = Flask(__name__)
 
-#----------------------------------------
-# launch
-#----------------------------------------
-
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
