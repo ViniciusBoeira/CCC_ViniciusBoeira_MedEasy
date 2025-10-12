30ba0169d78a9d116b6ada916475e9f16f6ae28a
Vinicius <vini.virtuoso28@gmail.com>
2025-10-12 19:15:02 -0300

    Alterei o modelo de usuário para incluir métodos de hash de senha e adicionado classes para Médico e Paciente com suas respectivas propriedades

--- app/models.py
+++ app/models.py
@@ -1,20 +1,26 @@
 from app import db
-
-class ModelExample(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(250))
-	content = db.Column(db.Text)
-	date = db.Column(db.DateTime)
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    user = db.Column(db.String(64), unique = True)
-    password = db.Column(db.String(500))
-    name = db.Column(db.String(500))
-    email = db.Column(db.String(120), unique = True)
-    # posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256))
+    name = db.Column(db.String(100), nullable=False)
+    # Discriminator para saber se o usuário é Médico ou Paciente
+    user_type = db.Column(db.String(50))
 
+    __mapper_args__ = {
+        'polymorphic_identity': 'user',
+        'polymorphic_on': user_type
+    }
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # Métodos exigidos pelo Flask-Login
     def is_authenticated(self):
         return True
 
@@ -28,4 +34,32 @@ class User(db.Model):
         return str(self.id)
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return f'<User {self.name}>'
+
+
+class Medico(User):
+    __tablename__ = 'medicos'
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    crm = db.Column(db.String(50), unique=True, nullable=False)
+    especialidade = db.Column(db.String(100))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'medico',
+    }
+
+    def __repr__(self):
+        return f'<Medico {self.name}, CRM: {self.crm}>'
+
+
+class Paciente(User):
+    __tablename__ = 'pacientes'
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    cpf = db.Column(db.String(11), unique=True, nullable=False)
+    data_nascimento = db.Column(db.DateTime)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'paciente',
+    }
+
+    def __repr__(self):
+        return f'<Paciente {self.name}, CPF: {self.cpf}>'
