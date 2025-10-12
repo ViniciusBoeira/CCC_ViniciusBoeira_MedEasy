02a950580373bb12095d72c96780192897f0df3d
Vinicius <vini.virtuoso28@gmail.com>
2025-10-12 19:12:34 -0300

    Alterei forms.py para atualizar classes de formulário e validadores para registro e login de usuários

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms  import TextField, TextAreaField, DateTimeField, PasswordField
-from wtforms.validators import Required
-
-class ExampleForm(FlaskForm):
-	title = TextField(u'Título', validators = [Required()])
-	content = TextAreaField(u'Conteúdo')
-	date = DateTimeField(u'Data', format='%d/%m/%Y %H:%M')
-	#recaptcha = RecaptchaField(u'Recaptcha')
+from wtforms import StringField, PasswordField, SubmitField, SelectField, DateField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
-	user = TextField(u'Usuário', validators = [Required()])
-	password = PasswordField(u'Senha', validators = [Required()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
+
+
+class CadastroPacienteForm(FlaskForm):
+    name = StringField('Nome Completo', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    cpf = StringField('CPF', validators=[DataRequired(), Length(min=11, max=11)])
+    data_nascimento = DateField('Data de Nascimento', format='%Y-%m-%d', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[
+        DataRequired(),
+        EqualTo('password2', message='As senhas devem ser iguais.')
+    ])
+    password2 = PasswordField('Confirme a Senha', validators=[DataRequired()])
+    submit = SubmitField('Cadastrar')
+
+
+class CadastroMedicoForm(FlaskForm):
+    name = StringField('Nome Completo', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    crm = StringField('CRM', validators=[DataRequired()])
+    especialidade = StringField('Especialidade', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[
+        DataRequired(),
+        EqualTo('password2', message='As senhas devem ser iguais.')
+    ])
+    password2 = PasswordField('Confirme a Senha', validators=[DataRequired()])
+    submit = SubmitField('Cadastrar')
