bae408f1c39ddfdee4be0760744c8dd2b6ec6c4d
Vinicius <vini.virtuoso28@gmail.com>
2025-10-12 19:10:13 -0300

    Alterei  __init__.py para importar o Flask-Migrate e inicializá-lo para migrações de bd

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,20 @@
-# -*- encoding: utf-8 -*-
-"""
-Python Aplication Template
-Licence: GPLv3
-"""
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_pymongo import PyMongo
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-#Configuration of application, see configuration.py, choose one and uncomment.
-#app.config.from_object('configuration.ProductionConfig')
 app.config.from_object('app.configuration.DevelopmentConfig')
-#app.config.from_object('configuration.TestingConfig')
 
 bs = Bootstrap(app) #flask-bootstrap
 db = SQLAlchemy(app) #flask-sqlalchemy
+migrate = Migrate(app, db)
 
 lm = LoginManager()
-lm.setup_app(app)
+lm.init_app(app)
 lm.login_view = 'login'
 
 from app import views, models
